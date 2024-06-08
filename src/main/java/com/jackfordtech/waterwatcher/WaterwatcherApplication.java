19c63ad273da3e36550c51886edd7efe1914cfcb
JackFord18 <jdford2@wisc.edu>
2024-06-08 15:25:12 -0500

    Added boilerplate code from spring boot initialization

---
+++ src/main/java/com/jackfordtech/waterwatcher/WaterwatcherApplication.java
@@ -0,0 +1,13 @@
+package com.jackfordtech.waterwatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterwatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterwatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jackfordtech/waterwatcher/WaterwatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackfordtech.waterwatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterwatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
