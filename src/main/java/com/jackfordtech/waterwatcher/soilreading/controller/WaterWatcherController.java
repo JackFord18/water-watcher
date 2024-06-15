173b888cc7a6e0ced2627f39ff4841568acd9e53
JackFord18 <jdford2@wisc.edu>
2024-06-15 18:47:06 -0500

    Enabled CORS for moisture reading GET requests

--- src/main/java/com/jackfordtech/waterwatcher/soilreading/controller/WaterWatcherController.java
+++ src/main/java/com/jackfordtech/waterwatcher/soilreading/controller/WaterWatcherController.java
@@ -3,10 +3,7 @@ package com.jackfordtech.waterwatcher.soilreading.controller;
 import com.jackfordtech.waterwatcher.soilreading.dao.SoilReading;
 import com.jackfordtech.waterwatcher.soilreading.service.SoilReadingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -26,6 +23,7 @@ public class WaterWatcherController {
 
     }
 
+    @CrossOrigin
     @GetMapping("/moisturereadings")
     public List<SoilReading> getSoilReadings(@RequestParam(name="date", required = false) LocalDate date,
                                              @RequestParam(name="starttime", required = false) LocalDateTime startDateTime,
